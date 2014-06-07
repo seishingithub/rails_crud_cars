c65d62a08f4b5852a31d121653e53153d9009d1f
Peggy Griffin <seishingithub@gmail.com>
2014-06-07 15:56:25 -0600

    User can edit and update car

--- spec/features/cars_spec.rb
+++ spec/features/cars_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Manage Cars' do
-  scenario 'User can create and view list of welcome' do
+  scenario 'User can create and view list of cars' do
     visit '/'
     expect(page).to have_content 'Welcome'
     click_on 'Add a car'
@@ -11,4 +11,28 @@ feature 'Manage Cars' do
     expect(page).to have_content 'Honda'
     expect(page).to have_content 'Civic'
   end
+
+  scenario 'User can edit and update car' do
+    visit '/'
+    click_on 'Add a car'
+    fill_in 'Make', with: 'Honda'
+    fill_in 'Model', with: 'Civic'
+    click_on 'Add car'
+    expect(page).to have_content 'Honda'
+    expect(page).to have_content 'Civic'
+    click_on 'Honda'
+    expect(page).to have_content 'Honda'
+    expect(page).to have_content 'Civic'
+    click_on 'Edit car'
+    fill_in 'Make', with: 'Toyota'
+    fill_in 'Model', with: 'Corolla'
+    click_on 'Update car'
+    expect(page).to have_content 'Toyota'
+    expect(page).to have_content 'Corolla'
+    expect(page).to have_no_content 'Honda'
+    expect(page).to have_no_content 'Civic'
+
+
+  end
+
 end
